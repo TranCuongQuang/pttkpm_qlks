257033c2009892aa52300b710866e1349600631f
Tran Quang Cuong <cuongtq@s-wifi.vn>
2020-02-23 22:19:35 +0700

    update

---
+++ QLKS/WebServiceCTQ.aspx.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using QLKS.Class;
+using System;
+
+namespace QLKS
+{
+    public partial class WebServiceCTQ : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var action = Request.Params["Action"];
+            Response.ContentType = "application/json; charset=utf-8";
+
+            switch (action)
+            {
+                #region Login
+
+                case "Login":
+                    Response.Write(JsonConvert.SerializeObject(Login()));
+                    Response.End();
+                    break;
+
+                #endregion Login
+
+                default:
+
+                    Response.End();
+                    break;
+            }
+        }
+
+        private AjaxReponseModel<dynamic> Login()
+        {
+            var response = new AjaxReponseModel<dynamic>(AjaxReponseStatusEnum.Success);
+
+            return response;
+        }
+    }
+}
