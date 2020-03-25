6f97139acf2302d479a2fb6b852202b6bfd49c0c
cuongph1196 <phancuong1196@gmail.com>
2020-03-26 00:01:47 +0700

    update

--- QLKS/EmployeeManagement.aspx.cs
+++ QLKS/EmployeeManagement.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace QLKS
 {
-    public partial class EmployeeManagement1 : System.Web.UI.Page
+    public partial class EmployeeManagement : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
