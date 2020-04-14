f85885440f6bdf1d9b7426d6619ef752f36bf416
Tran Quang Cuong <cuongtq@s-wifi.vn>
2020-04-14 22:07:16 +0700

    push menu

--- QLKS/Site.Master.cs
+++ QLKS/Site.Master.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace QLKS
 {
@@ -11,7 +7,32 @@ namespace QLKS
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                tblTaiKhoan user = Session["UserLogin"] as tblTaiKhoan;
+                lblUserLogin.InnerText = user.TenNV;
+                var html = "";
 
+                html += "<li class='active'><a href='EmployeeManagement.aspx'><i class='menu-icon fa fa-tachometer'></i><span class='menu-text'>Đặt phòng</span></a><b class='arrow'></b></li>" +
+                        "<li class=''><a href='EmployeeManagement.aspx'><i class='menu-icon fa fa-tachometer'></i><span class='menu-text'>Trả phòng</span></a><b class='arrow'></b></li>";
+
+                if (user.Quyen == "QuanLy")
+                {
+                    html += "<li class=''><a href='EmployeeManagement.aspx'><i class='menu-icon fa fa-tachometer'></i><span class='menu-text'>QL nhân viên</span></a><b class='arrow'></b></li>" +
+                                "<li class=''><a href='EmployeeManagement.aspx'><i class='menu-icon fa fa-tachometer'></i><span class='menu-text'>QL khách hàng</span></a><b class='arrow'></b></li>" +
+                                "<li class=''><a href='EmployeeManagement.aspx'><i class='menu-icon fa fa-tachometer'></i><span class='menu-text'>QL dịch vụ</span></a><b class='arrow'></b></li>" +
+                                "<li class=''><a href='EmployeeManagement.aspx'><i class='menu-icon fa fa-tachometer'></i><span class='menu-text'>QL phòng</span></a><b class='arrow'></b></li>" +
+                                "<li class=''><a href='EmployeeManagement.aspx'><i class='menu-icon fa fa-tachometer'></i><span class='menu-text'>QL trang thiết bị</span></a><b class='arrow'></b></li>" +
+                                "<li class=''><a href='EmployeeManagement.aspx'><i class='menu-icon fa fa-tachometer'></i><span class='menu-text'>QL trang thiết bị phòng</span></a><b class='arrow'></b></li>" +
+                                "<li class=''><a href='EmployeeManagement.aspx'><i class='menu-icon fa fa-tachometer'></i><span class='menu-text'>QL loại sản phẩm</span></a><b class='arrow'></b></li>";
+                }
+
+                html += "<li class=''><a href='EmployeeManagement.aspx'><i class='menu-icon fa fa-tachometer'></i><span class='menu-text'>Báo cáo doanh thu</span></a><b class='arrow'></b></li>" +
+                        "<li class=''><a href='EmployeeManagement.aspx'><i class='menu-icon fa fa-tachometer'></i><span class='menu-text'>Thống kê phòng</span></a><b class='arrow'></b></li>" +
+                        "<li class=''><a href='EmployeeManagement.aspx'><i class='menu-icon fa fa-tachometer'></i><span class='menu-text'>Thống kê nhân viên</span></a><b class='arrow'></b></li>";
+
+                listMenu.InnerHtml = html;
+            }
         }
     }
 }
--- QLKS/Site.Master.designer.cs
+++ QLKS/Site.Master.designer.cs
@@ -13,6 +13,24 @@ namespace QLKS {
     public partial class SiteMaster {
         
         /// <summary>
+        /// lblUserLogin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl lblUserLogin;
+        
+        /// <summary>
+        /// listMenu control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl listMenu;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
